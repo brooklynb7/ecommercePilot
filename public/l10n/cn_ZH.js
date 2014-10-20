a2f8d0ea89a285c8e57a56d442fcfbe24698c91e
Brooklyn Huang <brooklyn.huang@sap.com>
2014-10-20 18:34:36 +0800

    Initial project

---
+++ public/l10n/cn_ZH.js
@@ -0,0 +1,86 @@
+{
+	"header": {
+		"navbar": {
+			"UPLOAD": "上传",
+			"new": {
+				"NEW": "新建",
+				"PROJECT": "项目",
+				"TASK": "任务",
+				"USER": "用户",
+				"EMAIL": "邮件"
+			},
+			"NOTIFICATIONS": "通知"
+		}
+	},
+	"aside": {
+		"nav": {
+			"HEADER": "导航",
+			"ENTRY": "进入",
+			"MALL":"卖场",
+			"AGENCY":"经销商",
+			"BRAND":"品牌",
+			"DASHBOARD": "控制面板",
+			"CALENDAR": "日历",
+			"EMAIL": "邮件",
+			"APPS": "应用",
+			"GENERAL":"常规",
+			"YOUR_PROFILE": "个人信息",
+			"PRICE": "价格",
+			"SYSTEM":"系统",
+			"WIDGETS": "工具",
+			"SETTING":"设置",
+			"HELP":"帮助",
+			"components": {
+				"COMPONENTS": "组件",
+				"ui_kits": {
+					"UI_KITS": "界面工具",
+					"BUTTONS": "按钮",
+					"ICONS": "图标",
+					"GRID": "栅格",
+					"BOOTSTRAP": "Bootstrap",
+					"SORTABLE": "排序",
+					"PORTLET": "门户",
+					"TIMELINE": "时间线",
+					"VECTOR_MAP": "矢量地图"
+				},
+				"table": {
+					"TABLE": "表格",
+					"TABLE_STATIC": "Table static",
+					"DATATABLE": "Datatable",
+					"FOOTABLE": "Footable"
+				},
+				"form": {
+					"FORM": "Form",
+					"FORM_ELEMENTS": "Form elements",
+					"FORM_VALIDATION": "Form validation",
+					"FORM_WIZARD": "Form wizard"
+				},
+				"CHART": "Chart",
+				"pages": {
+					"PAGES": "Pages",
+					"PROFILE": "Profile",
+					"POST": "Post",
+					"SEARCH": "Search",
+					"INVOICE": "Invoice",
+					"LOCK_SCREEN": "Lock screen",
+					"SIGNIN": "Signin",
+					"SIGNUP": "Signup",
+					"FORGOT_PASSWORD": "Forgot password",
+					"404": "404"
+				}
+			},
+			"your_stuff": {
+				"YOUR_STUFF": "Your Stuff",
+				"PROFILE": "Profile",
+				"DOCUMENTS": "Documents"
+			}
+		},
+		"MILESTONE": "Milestone",
+		"RELEASE": "Release"
+	},
+	"agency":{
+		"SEARCH": "搜索经销商",
+		"ALL": "经销商列表",
+		"MANAGE": "经销商管理"
+	}
+}
