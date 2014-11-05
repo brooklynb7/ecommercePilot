78273a8209d68b236ce12631e01db0ec024f0d0a
brooklynb7 <brooklynb7@gmail.com>
2014-11-05 23:29:06 +0800

    Update searching brand

--- public/js/app/brand/brand.js
+++ public/js/app/brand/brand.js
@@ -1,139 +1,155 @@
-app.controller('BrandManageCtrl', ['$scope', 'BrandService', '$filter',
-	function($scope, BrandService, $filter) {
+(function() {
+	function getBrandList(BrandService, $scope, $filter) {
 		BrandService.getBrandList().then(function(items) {
 			$scope.items = items;
 			$scope.item = $filter('orderBy')($scope.items, 'first')[0];
 			$scope.item.selected = true;
 		});
+	}
 
-		$scope.selectItem = function(item) {
-			angular.forEach($scope.items, function(item) {
-				item.selected = false;
+	app.controller('BrandManageCtrl', ['$scope', 'BrandService', '$filter',
+		function($scope, BrandService, $filter) {
+			getBrandList(BrandService, $scope, $filter)
+
+			$scope.selectItem = function(item) {
+				angular.forEach($scope.items, function(item) {
+					item.selected = false;
+					item.editing = false;
+				});
+				$scope.item = item;
+				$scope.item.selected = true;
+			};
+
+			$scope.deleteItem = function(item) {
+				$scope.items.splice($scope.items.indexOf(item), 1);
+				$scope.item = $filter('orderBy')($scope.items, 'first')[0];
+				if ($scope.item) $scope.item.selected = true;
+			};
+
+			$scope.createItem = function() {
+				var item = {};
+				$scope.items.push(item);
+				$scope.selectItem(item);
+				$scope.item.editing = true;
+			};
+
+			$scope.editItem = function(item) {
+				if (item && item.selected) {
+					item.editing = true;
+				}
+			};
+
+			$scope.doneEditing = function(item) {
 				item.editing = false;
+			};
+
+		}
+	]);
+
+	app.controller('BrandExpandCityCtrl', ['$scope', 'BrandService', '$filter',
+		function($scope, BrandService, $filter) {
+			$scope.newCity = "";
+
+			getBrandList(BrandService, $scope, $filter)
+
+			$scope.selectItem = function(item) {
+				angular.forEach($scope.items, function(item) {
+					item.selected = false;
+				});
+				$scope.item = item;
+				$scope.item.selected = true;
+			};
+
+			$scope.deleteCity = function(city) {
+				$scope.item.expanding_cities.splice($scope.item.expanding_cities.indexOf(city), 1);
+			};
+
+			$scope.addCity = function(city) {
+				if (city) {
+					$scope.item.expanding_cities.push(city);
+					$scope.newCity = "";
+				}
+			};
+		}
+	]);
+
+	app.controller('BrandSellingCityCtrl', ['$scope', 'BrandService', '$filter',
+		function($scope, BrandService, $filter) {
+			$scope.newStore = {};
+
+			getBrandList(BrandService, $scope, $filter)
+
+			$scope.selectItem = function(item) {
+				angular.forEach($scope.items, function(item) {
+					item.selected = false;
+				});
+				$scope.item = item;
+				$scope.item.selected = true;
+			};
+
+			$scope.deleteStore = function(store) {
+				$scope.item.selling_cities.splice($scope.item.selling_cities.indexOf(store), 1);
+			};
+
+			$scope.addStore = function(store) {
+				if (store.name && store.city && store.address) {
+					$scope.item.selling_cities.push(store);
+					$scope.newStore = {};
+				}
+			};
+		}
+	]);
+
+	app.controller('BrandProductSeriesCtrl', ['$scope', 'BrandService', '$filter',
+		function($scope, BrandService, $filter) {
+			$scope.newProduct = {
+				decoration_styles: 1,
+				category: 1,
+				material_types: 1
+			};
+
+			getBrandList(BrandService, $scope, $filter)
+
+			$scope.selectItem = function(item) {
+				angular.forEach($scope.items, function(item) {
+					item.selected = false;
+				});
+				$scope.item = item;
+				$scope.item.selected = true;
+			};
+
+			$scope.deleteStore = function(store) {
+				$scope.item.selling_cities.splice($scope.item.selling_cities.indexOf(store), 1);
+			};
+
+			$scope.addProduct = function(product) {
+				if (product.name && product.short_description && product.description) {
+					$scope.item.productseries_set.push(product);
+					$scope.newProduct = {};
+				}
+			};
+		}
+	]);
+
+	app.controller('BrandSearchCtrl', ['$scope', 'BrandService', '$filter',
+		function($scope, BrandService, $filter) {
+			$scope.searchKey = "";
+			$scope.brandResults = [];
+
+			$scope.searchBrand = function(searchKey) {
+				BrandService.getBrandList().then(function(items) {
+					$scope.brandResults = items;
+				});
+			};
+		}
+	]);
+
+	app.controller('BrandDetailCtrl', ['$scope', 'BrandService', '$filter', '$stateParams',
+		function($scope, BrandService, $filter, $stateParams) {
+			$scope.brand = {};
+			BrandService.getBrand($stateParams.brandId).then(function(brand){
+				$scope.brand = brand;
 			});
-			$scope.item = item;
-			$scope.item.selected = true;
-		};
-
-		$scope.deleteItem = function(item) {
-			$scope.items.splice($scope.items.indexOf(item), 1);
-			$scope.item = $filter('orderBy')($scope.items, 'first')[0];
-			if ($scope.item) $scope.item.selected = true;
-		};
-
-		$scope.createItem = function() {
-			var item = {};
-			$scope.items.push(item);
-			$scope.selectItem(item);
-			$scope.item.editing = true;
-		};
-
-		$scope.editItem = function(item) {
-			if (item && item.selected) {
-				item.editing = true;
-			}
-		};
-
-		$scope.doneEditing = function(item) {
-			item.editing = false;
-		};
-
-	}
-]);
-
-app.controller('BrandExpandCityCtrl', ['$scope', 'BrandService', '$filter',
-	function($scope, BrandService, $filter) {
-		$scope.newCity = "";
-
-		BrandService.getBrandList().then(function(items) {
-			$scope.items = items;
-			$scope.item = $filter('orderBy')($scope.items, 'first')[0];
-			$scope.item.selected = true;
-		});
-
-		$scope.selectItem = function(item) {
-			angular.forEach($scope.items, function(item) {
-				item.selected = false;
-			});
-			$scope.item = item;
-			$scope.item.selected = true;
-		};
-
-		$scope.deleteCity = function(city) {
-			$scope.item.expanding_cities.splice($scope.item.expanding_cities.indexOf(city), 1);
-		};
-
-		$scope.addCity = function(city) {
-			if (city) {
-				$scope.item.expanding_cities.push(city);
-				$scope.newCity = "";
-			}
-		};
-	}
-]);
-
-app.controller('BrandSellingCityCtrl', ['$scope', 'BrandService', '$filter',
-	function($scope, BrandService, $filter) {
-		$scope.newStore = {};
-
-		BrandService.getBrandList().then(function(items) {
-			$scope.items = items;
-			$scope.item = $filter('orderBy')($scope.items, 'first')[0];
-			$scope.item.selected = true;
-		});
-
-		$scope.selectItem = function(item) {
-			angular.forEach($scope.items, function(item) {
-				item.selected = false;
-			});
-			$scope.item = item;
-			$scope.item.selected = true;
-		};
-
-		$scope.deleteStore = function(store) {
-			$scope.item.selling_cities.splice($scope.item.selling_cities.indexOf(store), 1);
-		};
-
-		$scope.addStore = function(store) {
-			if (store.name && store.city && store.address) {
-				$scope.item.selling_cities.push(store);
-				$scope.newStore = {};
-			}
-		};
-	}
-]);
-
-app.controller('BrandProductSeriesCtrl', ['$scope', 'BrandService', '$filter',
-	function($scope, BrandService, $filter) {
-		$scope.newProduct = {
-			decoration_styles: 1,
-			category: 1,
-			material_types: 1
-		};
-
-		BrandService.getBrandList().then(function(items) {
-			$scope.items = items;
-			$scope.item = $filter('orderBy')($scope.items, 'first')[0];
-			$scope.item.selected = true;
-		});
-
-		$scope.selectItem = function(item) {
-			angular.forEach($scope.items, function(item) {
-				item.selected = false;
-			});
-			$scope.item = item;
-			$scope.item.selected = true;
-		};
-
-		$scope.deleteStore = function(store) {
-			$scope.item.selling_cities.splice($scope.item.selling_cities.indexOf(store), 1);
-		};
-
-		$scope.addProduct = function(product) {
-			if (product.name && product.short_description && product.description) {
-				$scope.item.productseries_set.push(product);
-				$scope.newProduct = {};
-			}
-		};
-	}
-]);
+		}
+	]);
+}())
